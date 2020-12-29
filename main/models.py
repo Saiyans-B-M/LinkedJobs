4c3f6bb85aa14294fd04afe64dee958c30efb1d3
Farooq <mullafarooq4@gmail.com>
2020-12-29 13:26:30 +0530

    deployment changes

--- main/models.py
+++ main/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from datetime import datetime, timedelta
+
+#model manager here
+class MyModelManager(models.Manager):
+    def get_queryset(self):
+        now = datetime.now()
+        min_posted_on = now - timedelta(days=4)
+        return super(MyModelManager, self).get_queryset().filter(posted_on__gt=min_posted_on)
+
 
 # Create your models here.
 class Link(models.Model):
@@ -8,3 +17,7 @@ class Link(models.Model):
     link = models.URLField(max_length=500)
     logo = models.ImageField()
     posted_on = models.DateField(auto_now=True)
+    objects = MyModelManager()
+
+class Subscriber(models.Model):
+    email = models.EmailField(max_length=255)
