ac2e4a984e11dd2e60d43f52fb6b178b40aaceee
Farooq <mullafarooq4@gmail.com>
2020-12-30 13:54:17 +0530

    updated UI and subscriber functionality

--- main/views.py
+++ main/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render,HttpResponse
 from .models import Link
+from .forms import SubscriberForm
 
 # Create your views here.
 def home(request):
     template = 'home.html'
     obj = Link.objects.all()
+    form = SubscriberForm()
     if obj is not None:
-        o = {'objects':obj}
+        o = {'objects':obj,'form':form}
     else:
-        o = {'objects':None}
+        o = {'objects':None,'form':form}
+    if request.method == 'POST':
+        form = SubscriberForm(request.POST)
+        if form.is_valid:
+            form.save()
     return render(request,template,o)
     # return HttpResponse("Home here")
